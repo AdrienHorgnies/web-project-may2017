f4876a7914f0aefcffc533b82b7a063aebf90ec7
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2017-05-17 18:00:40 +0200

    configure testBest
    make all tests pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,46 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {RouterModule} from '@angular/router';
+import {ROUTES} from './app.route';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {HomeComponent} from './home/home.component';
+import {InformationComponent} from 'app/information/information.component';
+import {CoursesComponent} from 'app/courses/courses.component';
+import {NewsComponent} from './news/news.component';
+import {PartnershipsComponent} from './partnerships/partnerships.component';
+import {QualityComponent} from './quality/quality.component';
+import {ContactComponent} from './contact/contact.component';
+import {APP_BASE_HREF} from '@angular/common';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        FooterComponent,
+        InformationComponent,
+        CoursesComponent,
+        PartnershipsComponent,
+        NewsComponent,
+        QualityComponent,
+        ContactComponent,
+        HeaderComponent
       ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(ROUTES)
+      ],
+      providers: [
+        {provide: APP_BASE_HREF, useValue : '/' }
+      ]
     }).compileComponents();
   }));
 
