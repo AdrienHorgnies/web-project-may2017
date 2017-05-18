687178017dd23b8c5552bb7df41a8bc8c0b6cf0c
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2017-05-18 15:11:01 +0200

    create basic navigation menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  menuItems = [
+    {
+      route: '/accueil',
+      value: 'Accueil'
+    },
+    {
+      route: '/informations',
+      value: 'Informations'
+    },
+    {
+      route: '/formations',
+      value: 'Formations'
+    },
+    {
+      route: '/news',
+      value: 'News'
+    },
+    {
+      route: '/partenariats',
+      value: 'Partenariats'
+    },
+    {
+      route: '/qualite',
+      value: 'Qualite'
+    },
+    {
+      route: '/contact',
+      value: 'Contact'
+    }
+  ];
+  selectedMenuItem = this.menuItems[0].route;
+
+  selectThisMenuItem(menuItem: string) {
+    console.log(`From ${this.selectedMenuItem} to ${menuItem}`);
+    this.selectedMenuItem = menuItem;
+  }
 
   constructor() { }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title: string = 'Home Page';
+  body:  string = 'This is the about home body';
 
   constructor() { }
 
