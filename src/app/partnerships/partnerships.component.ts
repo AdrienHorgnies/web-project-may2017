a3b466b559c84a767f9848ec6834100368d6fa5a
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2017-05-21 17:22:43 +0200

    make partnerships page and upperCase header menu

--- src/app/partnerships/partnerships.component.ts
+++ src/app/partnerships/partnerships.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partnerships.component.css']
 })
 export class PartnershipsComponent implements OnInit {
+  partnerships = [
+    'Le Fonds social européen',
+    'Le Ministère de la Région wallone',
+    'Différentes administrations communales',
+    'La CSCSP',
+    'Le Ministère de la Fédération Wallonie-Bruxelles',
+    'L\'Université de Namur (convention REBOND)',
+    'La prison de Namur (convention REINSERT)',
+    'Le Parlement wallon',
+    'Le FOREM',
+    'Différents CPAS du namurois',
+    'La Croix-Rouge',
+    'L\'athénée royal de Jambes'
+  ];
 
   constructor() { }
 
