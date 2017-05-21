92774a2f6051798a384d65960f18997d4a5cfc86
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2017-05-21 16:51:08 +0200

    make galery

--- src/app/quality/quality.component.ts
+++ src/app/quality/quality.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quality.component.css']
 })
 export class QualityComponent implements OnInit {
+  pictures = Array(8).fill(0).map((value, idx) => `/assets/images/portfolio/75x75/g${idx + 1}.jpg`);
 
   constructor() { }
 
