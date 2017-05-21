04a10a4d33d7f50948558a57a481af8ee4c22e84
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2017-05-21 23:42:16 +0200

    achieve form double binding youhou

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+class ContactForm {
+  name: string;
+  firstname: string;
+  phone: string;
+  mail: string;
+  message: string;
+}
+
 @Component({
   selector: 'wp-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+  contactForm = new ContactForm();
 
   constructor() { }
 
   ngOnInit() {
+    this.contactForm.name = 'Horgnies';
   }
 
+  register() {
+    console.log(this.contactForm);
+  }
 }
