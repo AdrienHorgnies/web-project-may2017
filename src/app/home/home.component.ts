1e62d09dbfecc8cde840eb9aff17aa7ea47d695a
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2017-05-22 18:13:34 +0200

    add articles on homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NewsService} from '../news/news.service';
+import {ArticleModel} from '../news/article.model';
 
 @Component({
   selector: 'wp-home',
@@ -7,11 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title: string = 'Home Page';
-  body:  string = 'This is the about home body';
+  news: Array<ArticleModel> = [];
 
-  constructor() { }
+  constructor(private newsService: NewsService) { }
 
   ngOnInit() {
+    this.newsService.getNews().subscribe(
+      news => this.news = news
+    );
   }
 
 }
